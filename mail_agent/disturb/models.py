e56f1134b8da213835a3bd3d4d9f70e97089c9b5
Артём Ивлев <fordfocus2goga@gmail.com>
2023-02-20 22:01:09 +0400

    Added followers model

--- mail_agent/disturb/models.py
+++ mail_agent/disturb/models.py
@@ -4,3 +4,33 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Disturb(models.Model):
+    """Модель рассылки."""
+    pass
+
+
+class Follower(models.Model):
+    """Модель подписчкиков для рассылки."""
+    name = models.CharField(
+        verbose_name='Имя подписчика',
+        max_length=20,
+    )
+    surname = models.CharField(
+        verbose_name='Фамилия подписчика',
+        max_length=20,
+    )
+    b_day = models.DateField(
+        verbose_name='Дата рождения'
+    )
+    email = models.EmailField(
+        verbose_name='Почта подписчика',
+    )
+
+
+class Message(models.Model):
+    """Модель макета письма."""
+    maket = models.TextField(
+        verbose_name='Макет письма.'
+    )
--- mail_agent/users/models.py
+++ mail_agent/users/models.py
@@ -3,12 +3,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class CustomUser(AbstractUser):
-
+    """Переопределённая модель пользователя."""
     email = models.EmailField(
         verbose_name='User Email',
         unique=True,
         max_length=254,
     )
+
+
+@python_2_unicode_compatible
+class CustomUserfollow(models.Model):
+    owner = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='owners'
+    )
+    follower = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='followers',
+    )
+
